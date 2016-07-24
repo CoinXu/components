12d7cd4b8811d50a4321f05efb01d023331a37cb
Coin <duanxian0403@163.com>
2016-07-24 18:01:33 +0800

    fix pagination bugs

--- webpack.entry.js
+++ webpack.entry.js
@@ -7,20 +7,19 @@ const path = require('path');
 const componentsBasePath = path.join(process.cwd(), './dist');
 
 let _path = function (name) {
-    return path.join(componentsBasePath, name, '/demo/index');
+  return path.join(componentsBasePath, name, '/demo/index');
 };
 
 module.exports = {
-    Animate: _path('Animate'),
-    // Conditional: _path('Conditional'),
-    // Popup: _path('Popup'),
-    // HideOnBodyClick: _path('HideOnBodyClick'),
-    // Message: _path('Message'),
-    // Selectable: _path('Selectable'),
-    // Register: _path('register'),
-    // Collapse: _path('Collapse'),
-    // Calendar: _path('Calendar'),
-    // Slider: _path('Slider'),
-    // Pagination: _path('Pagination'),
-    // WebExcel: path.join(process.cwd(), './components/Test/Stick-Mark-Test/index')
+  Animate: _path('Animate'),
+  Conditional: _path('Conditional'),
+  Popup: _path('Popup'),
+  HideOnBodyClick: _path('HideOnBodyClick'),
+  Message: _path('Message'),
+  Selectable: _path('Selectable'),
+  Collapse: _path('Collapse'),
+  Calendar: _path('Calendar'),
+  Slider: _path('Slider'),
+  Pagination: _path('Pagination'),
+  WebExcel: path.join(process.cwd(), './components/Test/Stick-Mark-Test/index')
 };
